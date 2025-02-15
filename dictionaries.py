cf1bc252e3295233c25e25d39a7ba8bad1397360
DeadlyForces <krishkaslikar@yahoo.com>
2025-02-15 21:06:07 +0530

    Ok

---
+++ dictionaries.py
@@ -0,0 +1,16 @@
+# dictionary is a collection of key-value pairs
+
+capitals = {'USA': 'Washington, D.C.', 
+            'France': 'Paris', 
+            'India': 'New Delhi'}
+
+
+# print(dir(capitals))
+# print(help(capitals))
+# print(capitals.get("USA"))
+#if capitals.get("Japan"):
+#    print("That capital in the dictionary")
+#else:
+#    print("That capital is not in the dictionary")
+capitals.update({"Germany": "Berlin"})
+print(capitals)
