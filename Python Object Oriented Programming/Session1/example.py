b0a47247cc2bccdbe31caa2081496958672546b4
DeadlyForces <krishkaslikar@yahoo.com>
2025-03-04 18:29:07 +0530

    Add Car class and example usage

---
+++ Python Object Oriented Programming/Session1/example.py
@@ -0,0 +1,10 @@
+from car import Car
+
+car1 = Car("Toyota", 2019, "Red", True)
+car2 = Car("Honda", 2018, "Blue", False)
+car3 = Car("Ford", 2017, "Black", True)
+
+print(car1.model)
+print(car1.year)
+print(car1.color)
+print(car1.for_sale)
---
+++ Python Object Oriented Programming/desc.py
@@ -0,0 +1,4 @@
+# object = A "bundle"of related attributes (variables) and methods (functions)
+#          Ex. phone, cup, car, etc.
+#          You need a "class" to create many object
+# class = (blueprint) used to design the structure and layout of an object
